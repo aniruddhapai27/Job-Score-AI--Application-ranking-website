7fc21acd40d5585fc4540c1a38e7d08d130d7d80
aniruddhapai27 <149802807+aniruddhapai27@users.noreply.github.com>
2024-12-02 11:44:03 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,7 @@ const createSendToken = (user, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === "production",
   };
   res.cookie("jwt", token, cookieOptions);
