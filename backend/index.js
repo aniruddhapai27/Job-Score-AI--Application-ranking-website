fa2fc594fa4c178c68b71d2eaa1e8346bd624669
aniruddhapai27 <149802807+aniruddhapai27@users.noreply.github.com>
2024-12-02 12:05:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-dotenv.config({ path: "./.env" });
+dotenv.config();
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL, // Allow only this origin
