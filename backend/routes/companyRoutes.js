3de6b0ec9b27f02933a5051e183b177501ed3c95
aniruddhapai27 <149802807+aniruddhapai27@users.noreply.github.com>
2024-12-07 12:48:02 +0530

    Update companyRoutes.js

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -17,7 +17,7 @@ const router = express.Router();
 
 router.post("/create", isAuthenticated, createCompanyDetails);
 router.patch("/:companyId", isAuthenticated, editCompanyDetails);
-router.get("/get/:companyId", isAuthenticated, getCompanyDetails);
+router.get("/get", isAuthenticated, getCompanyDetails);
 
 router.post("/createJob", isAuthenticated, createJob);
 router.get("/getAllJobs", isAuthenticated, getAllJobs);
